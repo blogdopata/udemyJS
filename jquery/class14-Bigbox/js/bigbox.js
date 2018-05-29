fa636f6cedb91bc1195319d6df39d0db97b9b215
blogdopata <vcaballero_c@hotmail.com>
2018-05-28 22:12:00 -0500

    BigBoXEffect min 13:40

--- jquery/class14-Bigbox/js/bigbox.js
+++ jquery/class14-Bigbox/js/bigbox.js
@@ -2,18 +2,57 @@
 
 
 $.bigBox = function(opciones){
-
 	opciones = $.extend({
 
 
 	},opciones);
 
 
-	
+	var contenido = "";
 
-	console.log("vamos bien")
+			contenido = '<div class="bigBox-fondo"></div>';
+			contenido +=  '							<div class="bigBox-contenedor">';
+			contenido +=  '								<div class="bigBox-Cerrar"><i class="fa fa-times " ></i></div>';	
+			contenido +=  '								<div class="bigBox-circulo"><i class="fa fa-thumbs-o-up fa-4x"></i></div>';	
+			contenido +=  '								<div class="bigBox-contenido">';
+			contenido +=  '										<span class="bigBox-titulo">Genial!!</span>';
+			contenido +=  '										<span class="bigBox-texto">Estamos Listos para usar UDEMY</span>';
+			contenido +=  '								</div>';
+			contenido +=  '								<button class="bigBox-boton">Empezar Curso</button>';
+			contenido +=  '							</div>';
 
-};
+	$("body").append( contenido );
+
+
+
+
+
+
+	animar_entrada();
+
+
+	// animar la entrada
+	function animar_entrada(){
+
+
+		var 	$fondo = $(".bigBox-fondo");
+		var 	$bigBox = $(".bigBox-contenedor");
+				//	$fondo.fadeIn(300);
+
+					$fondo.show()	;
+					$bigBox.show();
+
+					var tl = new TimelineMax();
+							tl.to( $fondo,0.5,{opacity:0.3})
+								.to( $bigBox,0.5,{opacity:1},"-=0.5")
+								.from($bigBox,0.8,{y:"-=20",  ease: Bounce.easeOut },"-=0.5");
+
+
+	}
+
+
+}	;
+
 
 
 })();
