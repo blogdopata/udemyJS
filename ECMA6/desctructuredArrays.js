01e0c144741eb0198cf4a0af269845c23b92651c
blogdopata <vcaballero_c@hotmail.com>
2018-07-24 22:02:10 -0500

    fixAll Power VSCODE

---
+++ ECMA6/desctructuredArrays.js
@@ -0,0 +1,44 @@
+
+
+let frutas =  ["banado", "pera","manzada"];
+
+let [fruta1,frtuta2] = frutas;
+
+console.log(fruta1, frtuta2);
+
+let [,,fruta3] = frutas;
+console.log(fruta3);
+
+let otraFruit = "manzada";
+
+[,otraFruit] = frutas;
+
+console.log(otraFruit);
+
+  let a = 1;
+  let b = 2;
+  let temp;
+
+  temp = a;
+  a = b ;
+  b = temp;
+
+  console.log(a);
+  console.log(b);
+
+  [a,b] = [b,a];
+  console.log(a);
+  console.log(b);
+  
+  let arrObj =  [{a:10,b:20}, "LOL", 25];
+
+  [Zeta,x,y] = arrObj;
+  console.log(arrObj);
+  console.log(Zeta.a);
+  console.log(x);
+  
+  
+  
+
+  
+  
--- JSTime/arguments.js
+++ JSTime/arguments.js
@@ -5,7 +5,7 @@ function miFuncion () {
 	console.log(arguments[0] + arguments[2]);
 
 }
-
+	
 miFuncion(10,20,30);
 
 /* Aqui me exploto la cabeza */
