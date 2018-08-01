6341d5c5d42ff343f1f9a5213c3a68f7fb9739fb
blogdopata <vcaballero_c@hotmail.com>
2018-08-01 04:03:17 -0500

    SETS

--- ECMA6/setMethod.js
+++ ECMA6/setMethod.js
@@ -1,12 +1,25 @@
 
 
 let items = new Set([8,9]);
+console.log(  items instanceof Object);
 
 items.add(1);
 items.add(21);
-items.add(1);
-items.add(1);
+items.add(5);
+console.log(items);
+console.log(items.has(21));
 
+/* Borrar items*/
+items.delete(5)
+console.log(items);
+/* borrar todos los elementos del set*/
+
+items.clear();
 console.log(items);
 
-console.log(items.has(21));
+/* Convertir un set en un Array */
+
+let num = [1,2,3,4,5,6];
+let SetNum = new Set(num);
+console.log(SetNum);
+
