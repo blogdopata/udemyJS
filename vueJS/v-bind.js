1b734a70c317240a3ec7ee976a83de8ad08a79cf
blogdopata <vcaballero_c@hotmail.com>
2018-10-18 17:14:45 -0500

    v-bind or :attribute

---
+++ vueJS/v-bind.js
@@ -0,0 +1,12 @@
+
+
+var  app = new Vue({
+    el: "#vm",
+    data:{
+       imagen:"https://www.caracoltv.com/sites/default/files/styles/large_970x510/public/meme_nina_fuego_incendio_970.jpg?itok=ifzj97oM&timestamp=1539708959"
+    }
+
+
+
+
+})
