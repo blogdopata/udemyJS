0e8049f0d5bbffbdfa00f256ed9b105ce0b3b4f8
blogdopata <vcaballero_c@hotmail.com>
2018-10-22 03:27:20 -0500

    v-else-if

---
+++ vueJS/v-else-if.js
@@ -0,0 +1,22 @@
+
+
+var  app = new Vue({
+    el: "#vm",
+    data:{
+        mostrar:false,
+        parrafo:24
+    }
+})
+
+
+var a = 0.1;
+var b = 0.2;
+
+if(a+b == 0.3){
+    console.log("shi es cherto");
+    
+}else
+{
+    console.log("misssed");
+    
+}
