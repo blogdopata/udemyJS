05cf6aa9164be41713ecc69fc47418e6dd1c7841
blogdopata <vcaballero_c@hotmail.com>
2019-02-01 21:01:18 -0500

    templates tags

--- testJS/templateLiterals.js
+++ testJS/templateLiterals.js
@@ -1,6 +1,36 @@
 
+function etiqueta(literales, ...substituciones){
 
-let msj =" Vendo del literal"
+
+    let resultado = "";
+
+    console.log(literales);
+    console.log(substituciones);
+
+    literales[1] = 5;
+    console.log(literales[1]);
+    
+    for(let i = 0; i<substituciones.length;i++){
+
+        resultado+= literales[i];
+        resultado+= substituciones[i];
+
+    }
+    
+    return resultado;  
+}
 
 
-console.log(`Esa gente ${msj}`);
+
+
+
+
+let unidades = 5;
+let costo_unitario = 10;
+
+let mensaje = etiqueta`${unidades} lapices cuestan ${unidades*costo_unitario} dolares`;
+
+console.log(mensaje);
+
+
+
